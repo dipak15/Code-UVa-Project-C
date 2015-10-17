94c06bdd2f121d34d78c1ddc08ed083104bb5464
dipak15 <dipaknarayan@yahoo.com>
2015-10-17 06:25:53 -0400

    UVA project 100

---
+++ uva100.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+  int p, q, i, j, n, max, a, b;
+  
+  while (scanf("%d %d", &p, &q) == 2)
+  {
+	  if (p < q) {
+			a = p; b = q;
+	  } else {
+			a = q; b = p;
+		}
+
+	  max = 0;
+  	j = 0;
+  	for (i = a; i <= b; i++)
+  	{
+  	  j = 1;
+  	  n = i;
+	    while (n > 1)
+	    {
+	      ++j;
+	      if (n % 2) {
+	        n = (3 * n) + 1;
+  	    } else {
+          n = n / 2;
+        }
+      } /* end while */
+
+	    if (max < j) { max = j; }
+    } /* end for a to b */
+    printf("%d %d %d\n", p, q, max);
+  } /* end while scanf */
+  return 0;
+}
+
